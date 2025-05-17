1f49982a1443778a523cd322c05768f349cd42ad
hi01234 <earljohnfls107@gmail.com>
2025-05-17 20:37:21 +0800

    updated

--- 2nd Sem/Comprog2_StudentRecord_OOP_Class_List_Dictionaries_Objects.py
+++ 2nd Sem/Comprog2_StudentRecord_OOP_Class_List_Dictionaries_Objects.py
@@ -16,45 +16,47 @@ import os
         Display all
         Exit
 """""
+MAX=50
+class Student:
+    def __init__(self,filename="records.txt"):
+        self.records=[]
+        self.filename
+        self.load_accounts()
+    def AddRec(self):
 
-class Student_Record:
-    def __init__(self, filename="Record.txt"):
-        self.records = []
-        self.filename = filename
-        self.load_records()
-    def AddRec():
-        
-    def UpdRec():
+    def DelRec(self):
 
-    def DelRec():
+    def DispRec(self):
 
-    def DispRec():
+    def isFull(self):
+        return len(self.accounts) >= MAX
+    def locate(self,name):
+        for i, student in enumerate(self.records):
+            if student["name"]==name:
+                return i
+        return -1
 
 
-    def menu():
-        print("Student Record Program using python")
-        print("MENU")
-        print("   1. Add a Record")
-        print("   2. Update Record")
-        print("   3. Delete a Record")
-        print("   4. Display All")
-        print("   5. Exit")
-        op = int(input("Input choice (1-5): "))
-        return op
+def menu():
+    print("Student Record Program using python")
+    print("MENU")
+    print("   1. Add a Record")
+    print("   2. Delete a Record")
+    print("   3. Display All")
+    print("   4. Exit")
+    op = int(input("Input choice (1-4): "))
+    return op
 
 if __name__=="__main__":
     while True:
         match(menu):
             case 1:
-
                 AddRec()
             case 2:
-                UpdRec()
-            case 3:
                 DelRec()
-            case 4:
+            case 3:
                 DispRec()
-            case 5:
+            case 4:
                 break
             case _:
                 print("Invalid Choice!")
