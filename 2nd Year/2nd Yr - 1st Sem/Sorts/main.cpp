aa8ae4d130fe955042bca8a7b944d6318c7a1c6e
hi01234 <earljohnfls107@gmail.com>
2025-08-26 22:07:19 +0800

    Added Sorts Practice

--- 2nd Year/2nd Yr - 1st Sem/Sorts/main.cpp
+++ 2nd Year/2nd Yr - 1st Sem/Sorts/main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 void ibubble(int arr[],int len);
 void rbubble(int arr[],int len);
 
+void imerge(int arr[], int n);
+void imerge(int arr[], int l, int m, int r);
 void rmergeSort(int arr[],int l,int r);
 void rmerge(int arr[],int l,int mid,int r);
 
@@ -17,7 +19,6 @@ int main(){
     for(int i = 0;i <len;i++){
         cout<<arr[i]<<endl;
     }
-
 
     return 0;
 }
@@ -47,10 +48,42 @@ void rbubble(int arr[],int len){
             swap(arr[i], arr[i+1]);
         }
     }
-
     rbubble(arr,len-1);
 }
 
+void imerge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int a[n1];  //left subarray
+    int b[n2];  //right subarray
+
+    for (int i = 0; i < n1; i++) a[i] = arr[l + i];  //copy left
+    for (int i = 0; i < n2; i++) b[i] = arr[m + 1 + i]; //copy right
+
+    int i = 0, j = 0, k = l;
+
+    while (i < n1 && j < n2) {   //merge back to arr
+        if (a[i] < b[j]) arr[k++] = a[i++];
+        else arr[k++] = b[j++];
+    }
+
+    while (i < n1) arr[k++] = a[i++];  //copy remaining left
+    while (j < n2) arr[k++] = b[j++];  //copy remaining right
+}
+
+void imergesort(int arr[], int n) {
+    //size of subarrays to merge: 1, 2, 4, 8, ...
+    for (int curr_size = 1; curr_size <= n - 1; curr_size *= 2) {
+        //pick starting index of left subarray
+        for (int left_start = 0; left_start < n - 1; left_start += 2 * curr_size) {
+            int mid = min(left_start + curr_size - 1, n - 1);  //end of left
+            int right_end = min(left_start + 2 * curr_size - 1, n - 1); //end of right
+
+            imerge(arr, left_start, mid, right_end);  //merge two subarrays
+        }
+    }
+}
 
 void rmerge(int arr[],int l,int mid,int r){
     int n1= mid-l+1; //size of left & right subarray
